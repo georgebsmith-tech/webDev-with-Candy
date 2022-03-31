fbc7df6b23faaa71367b948884c25b40fc9dfd34
georgebsmith-tech <georgebsmith.tech@gmail.com>
2022-03-31 15:33:47 +0100

    made modifications

--- js/Dom/RollOn/index.js
+++ js/Dom/RollOn/index.js
@@ -17,37 +17,44 @@ let formContainerTwo = get(".form-container-2");
 let rollTwo = get(".roll-2");
 let scoreTwo = get(".score-2");
 let game = get(".game");
- let winner = get(".winner");
- let characTwo_Name = get(".character-2")
- let charac_Name = get(".character")
+let winner = get(".winner");
+let characTwo_Name = get(".character-2")
+let charac_Name = get(".character")
+
+let flag = true
 
 
 
- function final() {
-  
+function final() {
+  console.log("here")
 
-if (scoreOne.innerText === 100) {
-  game.innerText = "GAME OVER!!!";
-  winner.innerText = "PLAYER ONE WINS";
-  
-}
+  console.log(scoreOne)
+  console.log(scoreOne.innerText)
 
-else{
-  game.innerText = "";
-  game.innerText= "";
-}
+
+  if (scoreOne.innerText * 1 >= 100) {
+    game.innerText = "GAME OVER!!!";
+    winner.innerText = "PLAYER ONE WINS";
+    get(".modal").classList.remove("hide")
+
+  }
+
+  else {
+    game.innerText = "";
+    game.innerText = "";
+  }
 }
 
 function final_two() {
-  
-   if (scoreTwo.innnerText === 100) {
-  game.innerText = "GAME OVER!!!";
-  winner.innerText = "PLAYER TWO WINS";
-}
-else{
-  game.innerText = "";
-  game.innerText= "";
-}
+
+  if (scoreTwo.innnerText >= 100) {
+    game.innerText = "GAME OVER!!!";
+    winner.innerText = "PLAYER TWO WINS";
+  }
+  else {
+    game.innerText = "";
+    game.innerText = "";
+  }
 
 }
 
@@ -57,22 +64,23 @@ else{
 function character() {
   if (inputOneHolder.value === "") {
     charac_Name.innerText = "input a name!";
-    console.log(formContainerOne.classList.add("show"));
+
     // console.log(formContainerTwo.classList.remove("hide"));
   }
-  else{
+  else {
     charac_Name.innerText = "";
+    console.log(formContainerOne.classList.add("hide"));
   }
 }
 
 
 function character_two() {
-  if (inputTwoHolder.value ==="") {
+  if (inputTwoHolder.value === "") {
     characTwo_Name.innerText = "input a name!";
     console.log(formContainerTwo.classList.add("show"));
 
   }
-  else{
+  else {
     characTwo_Name.innerText = "";
   }
 }
@@ -83,8 +91,7 @@ enterBox.addEventListener("click", character);
 enterBox_2.addEventListener("click", character_two);
 
 
-inputTwoButton.addEventListener("click",() =>
-{
+inputTwoButton.addEventListener("click", () => {
   playerTwo.innerText = inputTwoHolder.value;
   console.log(formContainerTwo.classList.add("hide"));
 });
@@ -99,14 +106,57 @@ rollTwo.addEventListener("click", final_two, () => {
 inputOneButton.addEventListener("click", () => {
   playerOne.innerText = inputOneHolder.value;
   console.log(formContainerOne.classList);
-  console.log(formContainerOne.classList.add("hide"));
+  // console.log(formContainerOne.classList.add("hide"));
   console.log(formContainerOne.classList);
 });
 
-rollOne.addEventListener("click", final, () => {
-  let roll = 1 + Math.floor(Math.random() * 5);
-  scoreOne.innerText = scoreOne.innerText * 1 + roll;
+rollOne.addEventListener("click", () => {
+
+  if (flag === true) {
+
+
+    if (playerOne.textContent !== "") {
+      let roll = 1 + Math.floor(Math.random() * 5);
+      scoreOne.innerText = scoreOne.innerText * 1 + roll;
+      final()
+      flag = false
+    } else {
+      alert("Player one name must be entered!!")
+    }
+
+  } else {
+    alert("Allow Player Two na")
+  }
+
 });
 
+// inputOneHolder.addEventListener("input",(e)=>{
+//   console.log(e.target.value)
+//   if(e.target.value!==""){
+//     rollOne.disabled=false
+//   }else{
+//     rollOne.disabled=true
+
+//   }
+// })
+
+// inputOneHolder.addEventListener("input",(e)=>{
+//   console.log(e.target.value)
+//   if(e.target.value!==""){
+//     rollOne.disabled=false
+//   }else{
+//     rollOne.disabled=true
+
+//   }
+// })
+
+get(".modal").addEventListener("click", () => {
+  get(".modal").classList.add("hide")
+})
+
+
+get(".modal>div").addEventListener("click", (e) => {
+  e.stopPropagation()
+})
 
 
