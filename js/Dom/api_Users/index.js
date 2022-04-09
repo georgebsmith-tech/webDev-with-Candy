9b0a4ce6de11e4662248ee0ac6a1c01d058f0ac9
georgebsmith-tech <georgebsmith.tech@gmail.com>
2022-04-09 21:12:09 +0100

    added responsive design

--- js/Dom/api_Users/index.js
+++ js/Dom/api_Users/index.js
@@ -1,36 +1,48 @@
 let ul = document.querySelector("ul");
 console.log(ul);
 
-
-fetch ("https://jsonplaceholder.typicode.com/users")
-.then((result) => result.json())
+fetch("https://jsonplaceholder.typicode.com/users")
+  .then((result) => result.json())
   .then((data) => {
-let docFrag = new DocumentFragment();
-  data.forEach((item, index) =>
-  {
+    console.log(data);
+    let docFrag = new DocumentFragment();
+    data.forEach((item, index) => {
       let div = document.createElement("div");
-      if (index < 20) {
-        //   let div = document.createElement("div");
-          let li = document.createElement("li");
-        //   let img = document.createElement("img");
-        // img.src = item.url;
-        // li.append(img);
-          let p = document.createElement("p");
-        p.innerText = item.name;
-        li.append(p);
-        div.append(li);
-        docFrag.append(div);
-      }
-      if (index < 20) {
-          let li = document.createElement("li");
-          let p = document.createElement("p");
-           p.innerText = item.email;
-        li.append(p);
-        div.append(li);
-        docFrag.append(div);
-      }
-  });
-ul.append(docFrag);
-})
 
-.catch((err) => console.error(err));
+      //   let div = document.createElement("div");
+      let li = document.createElement("li");
+      //   let img = document.createElement("img");
+      // img.src = item.url;
+      // li.append(img);
+      let userHolder = document.createElement("div");
+      userHolder.className = "user-holder";
+      let p = document.createElement("p");
+      p.innerText = item.name;
+
+      let avatarHolder = document.createElement("p");
+      avatarHolder.innerText = item.name[0];
+
+      avatarHolder.className = "avatar";
+      userHolder.append(avatarHolder);
+      userHolder.append(p);
+      let p1 = document.createElement("p");
+      p1.innerText = item.email;
+      let p2 = document.createElement("p");
+      // p2.innerText = JSON.stringify(item.address);
+      p2.innerText = `Adrees:${item.address.suite}, ${item.address.street}, ${item.address.city}`;
+      li.append(userHolder);
+      li.append(p1);
+      li.append(p2);
+      docFrag.append(li);
+
+      // let li = document.createElement("li");
+      // let p = document.createElement("p");
+      // p.innerText = item.email;
+      // li.append(p);
+      // div.append(li);
+      // docFrag.append(div);
+    });
+    ul.append(docFrag);
+  })
+
+  .catch((err) => console.error(err));
--- js/Dom/apis-photos/index.js
+++ js/Dom/apis-photos/index.js
@@ -19,9 +19,33 @@ console.log(ul);
 //   })
 //   .catch((err) => console.error(err));
 
-fetch("https://jsonplaceholder.typicode.com/photos")
-  .then((result) => result.json())
-  .then((data) => {
+// fetch("https://jsonplaceholder.typicode.com/photos")
+//   .then((result) => result.json())
+//   .then((data) => {
+//     let frag = new DocumentFragment();
+//     data.forEach((item, idx) => {
+//       if (idx < 20) {
+//         let li = document.createElement("li");
+//         let img = document.createElement("img");
+//         img.src = item.url;
+//         li.append(img);
+//         let p = document.createElement("p");
+//         p.innerText = item.title;
+//         li.append(p);
+//         frag.append(li);
+//       };
+//        {
+
+//       }
+//     });
+//     ul.append(frag);
+//   })
+//   .catch((err) => console.error(err));
+
+async function getdata() {
+  try {
+    const resp = await fetch("https://jsonplaceholder.typicode.com/photos");
+    const data = await resp.json();
     let frag = new DocumentFragment();
     data.forEach((item, idx) => {
       if (idx < 20) {
@@ -33,11 +57,14 @@ fetch("https://jsonplaceholder.typicode.com/photos")
         p.innerText = item.title;
         li.append(p);
         frag.append(li);
-      };
-       {
-        
+      }
+      {
       }
     });
     ul.append(frag);
-  })
-  .catch((err) => console.error(err));
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+getdata();
